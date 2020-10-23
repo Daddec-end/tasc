7b52bb436465d249935a427603c5015126f31d8c
Sergiy <daddec@gmail.com>
2020-10-23 21:45:00 +0300

    first commit

---
+++ js/add.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// const obj = {
+//     firstName: 'Sergiy',
+//     lastName:  'Makal',
+//     age:       36,
+//     isMarried: true
+// };
+
+// console.log( obj.firstName );
+// console.log( obj["age"] );
+
+// let arr = ['dino.pmg', [], 'star.jpeg','tree,svg', 999, {}, 'tablу.pmg'];
+
+// let resault = prompt ('are you here');
+// console.log (typeof (resault));
+
+// let answers = [];
+// answers[0] = prompt( 'как ваше имя', '');
+// answers[1] = prompt( 'как ваша фамилия', '');
+// answers[2] = prompt( 'сколько вам лет', '');
+// console.log(typeof(null));
+// let name = prompt('как Ваше имя','');
+// alert(`Ваше имя - ${name}`);
+// console.log(4 + "5");
+let inkr = 10,
+    dekr = 10;
+
+
+for (let i = 0; i < 5; i++ ){
+    inkr++;
+    dekr--;  
+    let b = inkr + dekr;
+    alert(b);
+}
